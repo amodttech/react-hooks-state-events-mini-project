c98e02bf5ba0932f695b9fe570ce42fdba50deca
Amodttech <aaron@amodttech.com>
2021-01-13 15:39:05 -0500

    hmm

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Task from "./Task"
 
 function TaskList() {
+
+  // const tasksToDisplay = tasks
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {/* <Task text={} /> */}
     </div>
   );
 }
